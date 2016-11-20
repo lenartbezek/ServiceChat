9f7d7ce0e1dd161be9e53f32cb7acc90ef618a5b
Lenart Bezek <lench4991@gmail.com>
2016-11-20 21:32:34 +0100

    Create webconfig for authentication.

---
+++ ChatDB/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+
+namespace ChatDB
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute("Default",
+                "",
+                "~/Chat.aspx");
+        }
+    }
+}
