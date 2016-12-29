336af921e8bf2c4f1c878308c9b1b87b9fe407b3
LeonFRI <makoric.leon@gmail.com>
2016-12-29 11:49:22 +0100

    Login

--- AndroidStudioProject/ServiceChatTM/app/src/main/java/com/example/admin/servicechattm/model/LoginRequest.java
+++ AndroidStudioProject/ServiceChatTM/app/src/main/java/com/example/admin/servicechattm/model/LoginRequest.java
@@ -7,19 +7,15 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Naredi z nek request z username in pw ki ga pošlješ na nek url
- */
-
 public class LoginRequest extends StringRequest{
-    private static final String LOGIN_REQUEST_URL = "nevem ksn je link od servisa";
+    private static final String LOGIN_REQUEST_URL = "http://is-servicechat.azurewebsites.net/api/Login";
     private Map<String, String> params;
 
     public LoginRequest(String username, String password, Response.Listener<String> listener){
         super(Request.Method.POST, LOGIN_REQUEST_URL, listener, null);
         params = new HashMap<>();
-        params.put("username", username);
-        params.put("password", password);
+        params.put("Username", username);
+        params.put("Password", password);
     }
 
     @Override
--- AndroidStudioProject/ServiceChatTM/app/src/main/java/com/example/admin/servicechattm/ui/LoginActivity.java
+++ AndroidStudioProject/ServiceChatTM/app/src/main/java/com/example/admin/servicechattm/ui/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.example.admin.servicechattm.ui;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.example.admin.servicechattm.R;
+import com.example.admin.servicechattm.model.LoginRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final Button bLogin = (Button) findViewById((R.id.bLogin));
 
+        //listener za gumb login
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,29 +36,18 @@ public class LoginActivity extends AppCompatActivity {
                 final String username = etUsername.getText().toString();
                 final String password = etPassword.getText().toString();
 
-                //za testirat brez servisa
-                Intent intent = new Intent(LoginActivity.this, MessageActivity.class);
-                LoginActivity.this.startActivity(intent);
-
-                /*
-                response listener; se proba povezat na servis ki bi mu mogu vrnit json glede na vnesen username in pw (pac true/false). Ce rata ok gre na recycler view, cene vrze error uporabniku.
-                potem je se dodano da naj bi mu json vrnu se ime in priimek tko da ga lahko uporabim v recycler view
-                */
-
-/*
+                //response listener
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
+                            boolean success = jsonResponse.getBoolean("Success");
+                            //ce je success true gre na messageactivity
                             if(success){
-                                String name = jsonResponse.getString("name");
-                                String surname = jsonResponse.getString("surname");
                                 Intent intent = new Intent(LoginActivity.this, MessageActivity.class);
-                                intent.putExtra("name", name);
-                                intent.putExtra("surname", surname);
                                 LoginActivity.this.startActivity(intent);
+                                //ce ni prikaze uporabniku error
                             }else{
                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                                 builder.setMessage("Login failed, check your username and password.")
@@ -60,16 +58,14 @@ public class LoginActivity extends AppCompatActivity {
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
                     }
                 };
 
+                //naredimo request in ga zapakiramo v hashmap
                 LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                //naredimo volley queue in request vrzemo v volley queue
                 RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
                 queue.add(loginRequest);
-
-*/
-
 
             }
         });
