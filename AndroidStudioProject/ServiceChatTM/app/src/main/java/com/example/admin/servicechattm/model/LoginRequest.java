23f64b90ab994dba937edf50c234fcf97712b423
LeonFRI <makoric.leon@gmail.com>
2017-01-10 21:21:56 +0100

    meh

--- AndroidStudioProject/ServiceChatTM/app/src/main/java/com/example/admin/servicechattm/model/LoginRequest.java
+++ AndroidStudioProject/ServiceChatTM/app/src/main/java/com/example/admin/servicechattm/model/LoginRequest.java
@@ -1,25 +1,41 @@
 package com.example.admin.servicechattm.model;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginRequest extends StringRequest{
     private static final String LOGIN_REQUEST_URL = "http://is-servicechat.azurewebsites.net/api/Login";
-    private Map<String, String> params;
 
-    public LoginRequest(String username, String password, Response.Listener<String> listener){
+
+    public LoginRequest(String username, String password, Response.Listener<String> listener) {
         super(Request.Method.POST, LOGIN_REQUEST_URL, listener, null);
-        params = new HashMap<>();
-        params.put("Username", username);
-        params.put("Password", password);
     }
 
     @Override
-    public Map<String, String> getParams() {
-        return params;
+    public String getBodyContentType() {
+        return "application/json; charset=utf-8";
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        try {
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("Username", "admin");
+            jsonBody.put("Password", "Geslo.01");
+            final String requestBody = jsonBody.toString();
+            return requestBody == null ? null : requestBody.getBytes("utf-8");
+        } catch (Exception e) {
+            VolleyLog.wtf("error");
+            return null;
+        }
     }
 }
