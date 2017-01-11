9de0583c9ff18450fd8341175e8c71fb46230c51
Lenart Bezek <lench4991@gmail.com>
2017-01-11 18:48:53 +0100

    Sending messages.

--- AndroidStudioProject/ServiceChatTM/app/src/main/java/com/example/admin/servicechattm/MessageActivity.java
+++ AndroidStudioProject/ServiceChatTM/app/src/main/java/com/example/admin/servicechattm/MessageActivity.java
@@ -9,16 +9,22 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.example.admin.servicechattm.data.MessageRequest;
+import com.example.admin.servicechattm.data.SendRequest;
 import com.example.admin.servicechattm.data.UserRequest;
 import com.example.admin.servicechattm.model.Message;
 import com.example.admin.servicechattm.model.User;
+import org.joda.time.DateTime;
 
 import java.util.List;
 
-public class MessageActivity extends AppCompatActivity implements MessageRequest.ResponseListener, UserRequest.ResponseListener {
+public class MessageActivity extends AppCompatActivity
+        implements MessageRequest.ResponseListener, UserRequest.ResponseListener, SendRequest.ResponseListener{
 
     private final static long UPDATE_INTERVAL = 5000;
 
@@ -26,6 +32,9 @@ public class MessageActivity extends AppCompatActivity implements MessageRequest
     private String token;
 
     private RecyclerView recyclerView;
+    private EditText editText;
+    private Button sendButton;
+
     private MessageAdapter adapter;
     private Handler updateHandler;
     private Runnable updateData;
@@ -44,6 +53,14 @@ public class MessageActivity extends AppCompatActivity implements MessageRequest
             getSupportActionBar().show();
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        editText = (EditText) findViewById(R.id.etSendMessage);
+        sendButton = (Button) findViewById(R.id.bSend);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
 
         // Use a linear layout manager
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -79,6 +96,20 @@ public class MessageActivity extends AppCompatActivity implements MessageRequest
         };
 
         updateHandler.post(updateData);
+    }
+
+    private void sendMessage(){
+        String text = editText.getText().toString().trim();
+        editText.setText("");
+        if (text.length() > 0){
+            queue.add(SendRequest.create(text, token, MessageActivity.this));
+            Message myMessage = new Message();
+            myMessage.text = text;
+            myMessage.username = User.getMe().username;
+            myMessage.time = DateTime.now();
+            Message.set(myMessage);
+            adapter.notifyDataSetChanged();
+        }
     }
 
     @Override
@@ -145,6 +176,11 @@ public class MessageActivity extends AppCompatActivity implements MessageRequest
     public void onSuccessfulUserFetch(List<User> users) {
         User.set(users);
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onSuccessfulMessagePost() {
+        // PASS
     }
 
     @Override
